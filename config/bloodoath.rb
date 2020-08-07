1999031133a7eb0cc7acc92b879afeaa14654cac
gigglesluvscereal <dsg2386@gmail.com>
2020-08-07 10:40:51 -0500

    message

---
+++ config/bloodoath.rb
@@ -0,0 +1,6 @@
+class BloodOath
+
+
+
+    
+end#
---
+++ config/cult.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Cult
+
+@@all = []
+
+
+def initialize(name, location, founding_year, slogan)
+    @name = name
+    @location = location
+    @founding_year = founding_year
+    @slogan = slogan
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def self.find_by_name(name)
+    self.all.find do |cult|
+        cult.name == name
+    end
+end
+
+end#
